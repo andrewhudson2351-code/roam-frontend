681bf2d07a3ffacf3a8e1b3e822db52c1287146f
andrewhudson2351-code <andrew.hudson2351@gmail.com>
2026-03-20 21:00:33 -0400

    Replace App component with RoamApp component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import RoamApp from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RoamApp />
   </React.StrictMode>
 );
